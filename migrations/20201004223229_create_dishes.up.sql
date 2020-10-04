5ed78cff433401aeb8c52574e8e92237bfb681bb
zlobste <mykola.krainiuk@nure.ua>
2020-10-04 22:47:56 +0300

    Migrate DB: add 'dishes'

--- migrations/20201004223229_create_dishes.up.sql
+++ migrations/20201004223229_create_dishes.up.sql
@@ -2,9 +2,8 @@ CREATE TABLE dishes (
     id BIGSERIAL NOT NULL PRIMARY KEY,
     name VARCHAR NOT NULL,
     description VARCHAR NOT NULL,
-    cost money NOT NULL,
+    cost MONEY NOT NULL,
     menu_id BIGSERIAL NOT NULL,
     FOREIGN KEY (menu_id) REFERENCES menu (id) ON DELETE CASCADE
 );
 
-  
