858fefb33fb5f8ef365750e3baa75c7271de38ac
zlobste <mykola.krainiuk@nure.ua>
2020-10-04 22:28:12 +0300

    Migrate DB: add 'menu'

---
+++ migrations/20201004222502_create_menu.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE menu (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    title VARCHAR NOT NULL,
+    description VARCHAR NOT NULL UNIQUE,
+    organization_id BIGSERIAL NOT NULL,
+    FOREIGN KEY (organization_id) REFERENCES organizations (id) ON DELETE CASCADE
+);
