85db4df218a4b132b8911b9dd674c3f17acb2484
zlobste <mykola.krainiuk@nure.ua>
2020-10-05 23:50:39 +0300

    fix orders crud

--- internal/app/store/sqlstore/order_repository.go
+++ internal/app/store/sqlstore/order_repository.go
@@ -14,7 +14,7 @@ func (o *OrderRepository) Create(model *model.Order) error {
 	}
 	
 	return o.store.db.QueryRow(
-		"INSERT INTO order (cost, datetime, dish_id, user_id) VALUES ($1,$2, $3, $4) RETURNING id",
+		"INSERT INTO orders (cost, datetime, dish_id, user_id) VALUES ($1,$2, $3, $4) RETURNING id",
 		model.Cost,
 		model.DateTime,
 		model.DishId,
@@ -24,7 +24,7 @@ func (o *OrderRepository) Create(model *model.Order) error {
 
 func ( o *OrderRepository) FindById(id int64) (*model.Order, error) {
 	model := &model.Order{}
-	if err := o.store.db.QueryRow("SELECT id, title, description, organization_id FROM order WHERE id=$1", id).
+	if err := o.store.db.QueryRow("SELECT id, title, description, organization_id FROM orders WHERE id=$1", id).
 		Scan(&model.Id, &model.Cost, &model.DateTime, &model.DishId, &model.UserId); err != nil {
 		return nil, err
 	}
