6b3b5bd8193c7a50524954fb59ab0fddd57c7e5f
zlobste <mykola.krainiuk@nure.ua>
2020-10-07 00:08:09 +0300

    move user getter to auth handler

--- internal/app/apiserver/server/auth_handler.go
+++ internal/app/apiserver/server/auth_handler.go
@@ -14,6 +14,23 @@ var (
 	errNotAuthenticated         = errors.New("not authenticated")
 )
 
+func (s *server) GetUser() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tokenAuth, err := helpers.ExtractTokenMetadata(r)
+		if err != nil {
+			helpers.Error(w, r, http.StatusUnauthorized, err)
+			return
+		}
+		user, err := s.store.User().FindById(tokenAuth.UserId)
+		if err != nil {
+			helpers.Error(w, r, http.StatusUnauthorized, err)
+			return
+		}
+		user.Sanitize()
+		helpers.Respond(w, r, http.StatusCreated, user)
+	}
+}
+
 // Login handler for access
 func (s *server) SignIn() http.HandlerFunc {
 
