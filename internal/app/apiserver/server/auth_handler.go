9e90bb776a8600b67b77ec7dcf485948044768df
zlobste <mykola.krainiuk@nure.ua>
2020-10-07 00:05:22 +0300

    make common auth handler

--- internal/app/apiserver/server/auth_handler.go
+++ internal/app/apiserver/server/auth_handler.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	
 	"github.com/zlobste/mint-rest-api/internal/app/apiserver/helpers"
+	"github.com/zlobste/mint-rest-api/internal/app/models"
 )
 
 var (
@@ -47,5 +48,29 @@ func (s *server) SignIn() http.HandlerFunc {
 	}
 }
 
-
-
+// UserSignUp handler for creating new users
+func (s *server) SignUp() http.HandlerFunc {
+	
+	type request struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+	
+	return func(w http.ResponseWriter, r *http.Request) {
+		req := &request{}
+		if err := json.NewDecoder(r.Body).Decode(req); err != nil {
+			helpers.Error(w, r, http.StatusBadRequest, err)
+			return
+		}
+		u := &models.User{
+			Email: req.Email,
+			Password:req.Password,
+		}
+		if err := s.store.User().Create(u); err != nil {
+			helpers.Error(w, r, http.StatusUnprocessableEntity, err)
+			return
+		}
+		u.Sanitize()
+		helpers.Respond(w, r, http.StatusCreated, u)
+	}
+}
