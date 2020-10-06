b67b5a01b20f146a994de0275a646eb65690e90e
zlobste <mykola.krainiuk@nure.ua>
2020-10-07 00:37:50 +0300

    add menu routing

---
+++ internal/app/apiserver/server/menu_handler.go
@@ -0,0 +1,85 @@
+package server
+
+import (
+	"encoding/json"
+	"net/http"
+	
+	"github.com/zlobste/mint-rest-api/internal/app/apiserver/helpers"
+	"github.com/zlobste/mint-rest-api/internal/app/models"
+)
+
+func (s *server) CreateMenu() http.HandlerFunc {
+	
+	type request struct {
+		Title           string  `json:"title"`
+		Description     string  `json:"description"`
+		OrganizationId  string  `json:"organization_id"`
+	}
+	
+	return func(w http.ResponseWriter, r *http.Request) {
+		req := &request{}
+		if err := json.NewDecoder(r.Body).Decode(req); err != nil {
+			helpers.Error(w, r, http.StatusBadRequest, err)
+			return
+		}
+		m := &models.Menu{
+			Title: req.Title,
+			Description: req.Description,
+			OrganizationId: req.OrganizationId,
+		}
+		
+		if err := s.store.Menu().Create(m); err != nil {
+			helpers.Error(w, r, http.StatusUnprocessableEntity, err)
+			return
+		}
+
+		helpers.Respond(w, r, http.StatusCreated, m)
+	}
+}
+
+func (s *server) DeleteMenu() http.HandlerFunc {
+	
+	type request struct {
+		id int64 `json:"id"`
+	}
+	
+	return func(w http.ResponseWriter, r *http.Request) {
+		req := &request{}
+		if err := json.NewDecoder(r.Body).Decode(req); err != nil {
+			helpers.Error(w, r, http.StatusBadRequest, err)
+			return
+		}
+		
+		err := s.store.Menu().DeleteById(req.id)
+		if err != nil {
+			helpers.Error(w, r, http.StatusUnauthorized, err)
+			return
+		}
+		
+		helpers.Respond(w, r, http.StatusOK, nil)
+	}
+}
+
+func (s *server) GetMenu() http.HandlerFunc {
+	
+	type request struct {
+		id int64 `json:"id"`
+	}
+	
+	return func(w http.ResponseWriter, r *http.Request) {
+		
+		req := &request{}
+		if err := json.NewDecoder(r.Body).Decode(req); err != nil {
+			helpers.Error(w, r, http.StatusBadRequest, err)
+			return
+		}
+		
+		user, err := s.store.Menu().FindById(req.id)
+		if err != nil {
+			helpers.Error(w, r, http.StatusUnauthorized, err)
+			return
+		}
+
+		helpers.Respond(w, r, http.StatusOK, user)
+	}
+}
