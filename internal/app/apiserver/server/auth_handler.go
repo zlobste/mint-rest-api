fd7af13ad328a8eebfc09b28e78948488d11caf7
zlobste <nkrayniuk@gmail.com>
2020-10-07 09:56:40 +0300

    add common auth

--- internal/app/apiserver/server/auth_handler.go
+++ internal/app/apiserver/server/auth_handler.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"errors"
 	"net/http"
-	
+
 	"github.com/zlobste/mint-rest-api/internal/app/apiserver/helpers"
 	"github.com/zlobste/mint-rest-api/internal/app/models"
 )
@@ -20,8 +20,9 @@ func (s *server) SignIn() http.HandlerFunc {
 	type request struct {
 		Email string    `json:"email"`
 		Password string `json:"password"`
+		Type string		`json:"type"`
 	}
-	
+
 	type JWT struct {
 		Token string `json:"token"`
 	}
@@ -32,45 +33,71 @@ func (s *server) SignIn() http.HandlerFunc {
 			helpers.Error(w, r, http.StatusBadRequest, err)
 			return
 		}
-		user, err := s.store.User().FindByEmail(req.Email)
-		if err != nil || !user.ComparePassword(req.Password) {
-			helpers.Error(w, r, http.StatusUnauthorized, errIncorrectEmailOrPassword)
-			return
+
+		var id int64
+		if req.Type == "user" {
+			user, err := s.store.User().FindByEmail(req.Email)
+			if err != nil || !user.ComparePassword(req.Password) {
+				helpers.Error(w, r, http.StatusUnauthorized, errIncorrectEmailOrPassword)
+				return
+			}
+			id = user.Id
+		} else {
+			organizatin, err := s.store.User().FindByEmail(req.Email)
+			if err != nil || !organizatin.ComparePassword(req.Password) {
+				helpers.Error(w, r, http.StatusUnauthorized, errIncorrectEmailOrPassword)
+				return
+			}
+			id = organizatin.Id
 		}
 
-		token, err := helpers.CreateJWT(user.Id)
+		token, err := helpers.CreateJWT(id)
 		if err != nil {
 			helpers.Error(w, r, http.StatusBadRequest, err)
 			return
 		}
-		
+
 		helpers.Respond(w, r, http.StatusOK, &JWT{Token:token})
 	}
 }
 
 // UserSignUp handler for creating new users
 func (s *server) SignUp() http.HandlerFunc {
-	
 	type request struct {
 		Email    string `json:"email"`
 		Password string `json:"password"`
+		Type string		`json:"type"`
 	}
-	
+
 	return func(w http.ResponseWriter, r *http.Request) {
 		req := &request{}
 		if err := json.NewDecoder(r.Body).Decode(req); err != nil {
 			helpers.Error(w, r, http.StatusBadRequest, err)
 			return
 		}
-		u := &models.User{
-			Email: req.Email,
-			Password:req.Password,
+
+		if req.Type == "user" {
+			u := &models.User{
+				Email: req.Email,
+				Password:req.Password,
+			}
+			if err := s.store.User().Create(u); err != nil {
+				helpers.Error(w, r, http.StatusUnprocessableEntity, err)
+				return
+			}
+			u.Sanitize()
+			helpers.Respond(w, r, http.StatusCreated, u)
+		} else {
+			o := &models.Organization{
+				Email: req.Email,
+				Password:req.Password,
+			}
+			if err := s.store.Organization().Create(o); err != nil {
+				helpers.Error(w, r, http.StatusUnprocessableEntity, err)
+				return
+			}
+			o.Sanitize()
+			helpers.Respond(w, r, http.StatusCreated, o)
 		}
-		if err := s.store.User().Create(u); err != nil {
-			helpers.Error(w, r, http.StatusUnprocessableEntity, err)
-			return
-		}
-		u.Sanitize()
-		helpers.Respond(w, r, http.StatusCreated, u)
 	}
 }
---
+++ internal/app/apiserver/server/organization_handler.go
@@ -0,0 +1,23 @@
+package server
+
+import (
+	"github.com/zlobste/mint-rest-api/internal/app/apiserver/helpers"
+	"net/http"
+)
+
+func (s *server) GetOrganization() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tokenAuth, err := helpers.ExtractTokenMetadata(r)
+		if err != nil {
+			helpers.Error(w, r, http.StatusUnauthorized, err)
+			return
+		}
+		organization, err := s.store.Organization().FindById(tokenAuth.UserId)
+		if err != nil {
+			helpers.Error(w, r, http.StatusUnauthorized, err)
+			return
+		}
+		organization.Sanitize()
+		helpers.Respond(w, r, http.StatusCreated, organization)
+	}
+}
