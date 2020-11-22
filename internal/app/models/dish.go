d6f13561f1a8bf85daab5ed9bf54626b558b1082
zlobste <mykola.krainiuk@nure.ua>
2020-11-22 18:56:06 +0200

    Update dish model.

--- internal/app/models/dish.go
+++ internal/app/models/dish.go
@@ -6,15 +6,16 @@ import (
 
 type Dish struct {
 	Id              int64   `json:"id"`
-	Name            string  `json:"name"`
+	Title           string  `json:"title"`
 	Description     string  `json:"description"`
 	Cost            float64 `json:"cost"`
+	Disabled        bool    `json:"disabled"`
 }
 
 func (d *Dish) Validate() error {
 	return validation.ValidateStruct(
 		d,
-		validation.Field(&d.Name, validation.Required, validation.Length(6, 100)),
+		validation.Field(&d.Title, validation.Required, validation.Length(6, 100)),
 		validation.Field(&d.Description, validation.Required, validation.Length(6, 2000)),
 		validation.Field(&d.Cost, validation.Required),
 		validation.Field(&d.Cost, validation.Required),
